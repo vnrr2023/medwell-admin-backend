77cec62cc3376c42c33c582ff101e8419bcd7cfd
rsayyed591 <rehansayyed591@gmail.com>
2025-03-01 23:01:39 +0530

    Updated Server files

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const connectDB = require("./db/db");
 const userRoutes = require("./routes/userRoutes");
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 
 connectDB();
 
-app.use("/api/users", userRoutes); // Register user routes
+app.use("/api/users", userRoutes);
 
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const app = require("./app");
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
