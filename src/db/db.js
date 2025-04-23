ff624056bb08a954be15ed3a1baf86a2b0df7add
rsayyed591 <rehansayyed591@gmail.com>
2025-04-23 19:47:03 +0530

    updated db

--- src/db/db.js
+++ src/db/db.js
@@ -1,13 +1,19 @@
 const postgres = require("postgres");
 require("dotenv").config();
 
-const sql = postgres(process.env.DATABASE_URL, {
-  ssl: "require", // Ensures SSL is used for Supabase connections
+const sql = postgres({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  ssl: { rejectUnauthorized: false }
 });
+
 
 const connectDB = async () => {
   try {
-    await sql`SELECT 1`; // Simple query to check connection
+    await sql`SELECT 1`;
     console.log("✅ PostgreSQL Connected");
   } catch (error) {
     console.error("❌ PostgreSQL Connection Failed:", error);
